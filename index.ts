c734bad83047f720e917e890b384fc2b0df9e6b1
emmywise <adefila.emmywise@gmail.com>
2024-01-20 16:23:04 +0000

    added maxArea function

---
+++ index.ts
@@ -0,0 +1,19 @@
+function maxArea(height: number[]): number {
+    let maxArea = 0;
+    let left = 0;
+    let right = height.length - 1;
+
+    while (left < right) {
+        const minHeight = Math.min(height[left], height[right]);
+        const currentArea = (right - left) * minHeight;
+        maxArea = Math.max(maxArea, currentArea);
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return maxArea;
+}
